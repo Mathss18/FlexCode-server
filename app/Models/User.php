529742ff09c50a677ee2454fb399b922a312577f
unknown <theus.7@hotmail.com>
2021-10-09 21:22:55 -0300

    Iniciando auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,8 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    // public function getAuthPassword()
-    // {
-    //     return $this->senha;
-    // }
+    public function getAuthPassword()
+    {
+        return $this->senha;
+    }
 }
