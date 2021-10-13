dfb2f95874ad19696196ed6a0897cd724e5349d7
unknown <theus.7@hotmail.com>
2021-10-13 20:27:25 -0300

    Ajuste controller de Grupos

--- app/Http/Controllers/GrupoController.php
+++ app/Http/Controllers/GrupoController.php
@@ -26,22 +26,22 @@ class GrupoController extends Controller
         $grupo = new Grupo;
         $grupo->nome = $request->input('nome');
 
-        $grupo->nome = $request->input('domingo');
-        $grupo->nome = $request->input('segunda');
-        $grupo->nome = $request->input('terca');
-        $grupo->nome = $request->input('quarta');
-        $grupo->nome = $request->input('quinta');
-        $grupo->nome = $request->input('sexta');
-        $grupo->nome = $request->input('sabado');
+        $grupo->domingo = $request->input('domingo');
+        $grupo->segunda = $request->input('segunda');
+        $grupo->terca = $request->input('terca');
+        $grupo->quarta = $request->input('quarta');
+        $grupo->quinta = $request->input('quinta');
+        $grupo->sexta = $request->input('sexta');
+        $grupo->sabado = $request->input('sabado');
 
-        $grupo->nome = $request->input('horaInicio');
-        $grupo->nome = $request->input('horaFim');
+        $grupo->horaInicio = $request->input('horaInicio');
+        $grupo->horaFim = $request->input('horaFim');
 
-        $grupo->nome = $request->input('clientes');
-        $grupo->nome = $request->input('fornecedores');
-        $grupo->nome = $request->input('grupos');
-        $grupo->nome = $request->input('transportadoras');
-        $grupo->nome = $request->input('usuarios');
+        $grupo->clientes = $request->input('clientes');
+        $grupo->fornecedores = $request->input('fornecedores');
+        $grupo->grupos = $request->input('grupos');
+        $grupo->transportadoras = $request->input('transportadoras');
+        $grupo->usuarios = $request->input('usuarios');
 
 
         if ($grupo->save()) {
@@ -53,7 +53,7 @@ class GrupoController extends Controller
     {
         $grupo = Grupo::findOrFail($request->id);
         $grupo->nome = $request->input('nome');
-        
+
         $grupo->nome = $request->input('domingo');
         $grupo->nome = $request->input('segunda');
         $grupo->nome = $request->input('terca');
