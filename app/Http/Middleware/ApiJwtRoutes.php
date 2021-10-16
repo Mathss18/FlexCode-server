9c45d79170ccb5a2f94c595ecc465a8c668de1f1
unknown <theus.7@hotmail.com>
2021-10-16 17:38:06 -0300

    Configurando Status code token invalido/expirado/nao encontrado

--- app/Http/Middleware/ApiJwtRoutes.php
+++ app/Http/Middleware/ApiJwtRoutes.php
@@ -24,11 +24,11 @@ class ApiJwtRoutes extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $exception) {
             if ($exception instanceof TokenInvalidException) {
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['status' => 'Token is Invalid'], 401);
             } else if ($exception instanceof TokenExpiredException) {
-                return response()->json(['status' => 'Token is Expired']);
+                return response()->json(['status' => 'Token is Expired'], 401);
             } else {
-                return response()->json(['status' => 'Authorization Token not found']);
+                return response()->json(['status' => 'Authorization Token not found'], 401);
             }
         }
         return $next($request);
