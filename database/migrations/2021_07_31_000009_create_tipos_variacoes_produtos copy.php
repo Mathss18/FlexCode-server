f4b8e599d5b2c91d008002cc005325447b36b4fb
unknown <theus.7@hotmail.com>
2021-10-28 19:34:51 -0300

    Criação modulos opcoes adicionais de produtos

--- database/migrations/2021_07_31_000009_create_tipos_variacoes_produtos copy.php
+++ database/migrations/2021_07_31_000009_create_tipos_variacoes_produtos copy.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NomesVariacoesProdutosTable extends Migration
+class CreateTiposVariacoesProdutosTable extends Migration
 {
     /**
      * Run the migrations.
