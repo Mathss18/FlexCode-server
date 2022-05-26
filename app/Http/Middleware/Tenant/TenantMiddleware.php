3ab6ac2020ec3a77b1c8d2c7353b847366800ecb
Matheus <matheus.filho@mtcorp.com.br>
2022-05-26 17:48:13 -0300

    adm tenant

--- app/Http/Middleware/Tenant/TenantMiddleware.php
+++ app/Http/Middleware/Tenant/TenantMiddleware.php
@@ -19,7 +19,7 @@ class TenantMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // return response()->json($request->getHost(),500);
+        dd(response()->json($request->getHost(),500));
         $manager = app(ManagerTenant::class);
 
         if ($manager->isAdmDomain()) {
