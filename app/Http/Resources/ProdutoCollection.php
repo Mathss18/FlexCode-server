8b1e34ab502ad4560269922cd04af90c5f81e1c2
unknown <theus.7@hotmail.com>
2022-06-06 21:51:18 -0300

    using collection to increase the produtos  performance

--- app/Http/Resources/ProdutoCollection.php
+++ app/Http/Resources/ProdutoCollection.php
@@ -20,16 +20,14 @@ class ProdutoCollection extends JsonResource
             'nome' => $this->nome,
             'codigoInterno' => $this->codigoInterno,
             'grupo_produto' => [
-                'id' => $this->grupo_produto->id,
                 'nome' => $this->grupo_produto->nome,
             ],
             'custoFinal' => $this->custoFinal,
             'quantidadeAtual' => $this->quantidadeAtual,
             'cliente' => [
-                'id' => $this->cliente->id ?? null,
                 'nome' => $this->cliente->nome ?? null,
             ],
-            'fornecedores' => collect($this->fornecedores)->pluck('nome'),
+            'fornecedores' => collect($this->fornecedores->nome),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
 
