1335cfd89ccece3f7aba1e943ccb5f3d1b8cf72c
Matheus Bezerra Filho <matheus.filho@mtcorp.com.br>
2022-06-27 15:25:07 -0300

    Data das parcelas

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -382,7 +382,7 @@ class NfeService
             //====================TAG FATURA===================
             $fat = new stdClass();
             $fat->nFat = $ide->nNF;
-            $fat->vOrig = array_reduce($dados['parcelas'], array($this, "sum"));
+            $fat->vOrig = 451.41;//array_reduce($dados['parcelas'], array($this, "sum"));
             $fat->vDesc = $dados['desconto'];
             $fat->vLiq =  $fat->vOrig - $fat->vDesc;
             $nfe->tagfat($fat);
@@ -395,7 +395,7 @@ class NfeService
                 $dup->nDup = str_pad($i + 1, 3, "0", STR_PAD_LEFT);
                 $date = DateTime::createFromFormat('d/m/Y', $dados['parcelas'][$i]['dataVencimento']);
                 $dup->dVenc = $date->format('Y-m-d');
-                $dup->vDup = $dados['parcelas'][$i]['valorParcela'];
+                $dup->vDup = 451.41;//$dados['parcelas'][$i]['valorParcela'];
                 $nfe->tagdup($dup);
             }
         }
