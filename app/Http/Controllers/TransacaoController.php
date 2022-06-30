8f292779f3b3271c66a67da5248c7b37b25230d0
Matheus Bezerra Filho <matheus.filho@mtcorp.com.br>
2022-06-30 10:49:12 -0300

    order by id again

--- app/Http/Controllers/TransacaoController.php
+++ app/Http/Controllers/TransacaoController.php
@@ -17,7 +17,7 @@ class TransacaoController extends Controller
             $from = date($request->query('dataInicio'));
             $to = date($request->query('dataFim'));
 
-            $transacoes = Transacao::with(['conta_bancaria', 'compra', 'venda'])->whereBetween('data', [$from, $to])->orderBy('data', 'ASC')->orderBy('dataTransacaoRegistrada', 'ASC')->get();
+            $transacoes = Transacao::with(['conta_bancaria', 'compra', 'venda'])->whereBetween('data', [$from, $to])->orderBy('id', 'desc')->get();
             $response = APIHelper::APIResponse(true, 200, 'Sucesso', $transacoes);
             return response()->json($response, 200);
         } catch (Exception  $ex) {
