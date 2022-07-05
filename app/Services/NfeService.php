d32ca4193c0e1c377a6efc7d1006b46bb8e20e2c
Matheus Bezerra Filho <matheus.filho@mtcorp.com.br>
2022-07-05 09:27:49 -0300

    valor

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -388,34 +388,34 @@ class NfeService
             $nfe->tagfat($fat);
             //====================TAG DUPLICATA===================
 
-            // for ($i = 0; $i < count($dados['parcelas']); $i++) {
+            for ($i = 0; $i < count($dados['parcelas']); $i++) {
 
-            //     $dup = new stdClass();
+                $dup = new stdClass();
 
-            //     $dup->nDup = str_pad($i + 1, 3, "0", STR_PAD_LEFT);
-            //     $date = DateTime::createFromFormat('d/m/Y', $dados['parcelas'][$i]['dataVencimento']);
-            //     $dup->dVenc = $date->format('Y-m-d');
-            //     $dup->vDup = $dados['parcelas'][$i]['valorParcela'];
-            //     $nfe->tagdup($dup);
-            // }
+                $dup->nDup = str_pad($i + 1, 3, "0", STR_PAD_LEFT);
+                $date = DateTime::createFromFormat('d/m/Y', $dados['parcelas'][$i]['dataVencimento']);
+                $dup->dVenc = $date->format('Y-m-d');
+                $dup->vDup = $dados['parcelas'][$i]['valorParcela'];
+                $nfe->tagdup($dup);
+            }
 
-            $dup1 = new stdClass();
-            $dup1->nDup = '001';
-            $dup1->dVenc = '2022-08-04';
-            $dup1->vDup = 2361.28;
-            $nfe->tagdup($dup1);
+            // $dup1 = new stdClass();
+            // $dup1->nDup = '001';
+            // $dup1->dVenc = '2022-07-27';
+            // $dup1->vDup = 6323.88;
+            // $nfe->tagdup($dup1);
 
-            $dup2 = new stdClass();
-            $dup2->nDup = '002';
-            $dup2->dVenc = '2022-08-19';
-            $dup2->vDup = 2361.28;
-            $nfe->tagdup($dup2);
+            // $dup2 = new stdClass();
+            // $dup2->nDup = '002';
+            // $dup2->dVenc = '2022-08-03';
+            // $dup2->vDup = 6323.88;
+            // $nfe->tagdup($dup2);
 
-            $dup3 = new stdClass();
-            $dup3->nDup = '003';
-            $dup3->dVenc = '2022-09-03';
-            $dup3->vDup = 2361.29;
-            $nfe->tagdup($dup3);
+            // $dup3 = new stdClass();
+            // $dup3->nDup = '003';
+            // $dup3->dVenc = '2022-08-10';
+            // $dup3->vDup = 6323.88;
+            // $nfe->tagdup($dup3);
 
             // $dup4 = new stdClass();
             // $dup4->nDup = '004';
