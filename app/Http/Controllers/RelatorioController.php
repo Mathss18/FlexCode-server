b2a554c7c848ab02d4d977abbb3a0426fe378d9c
unknown <theus.7@hotmail.com>
2022-10-23 01:52:19 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -210,11 +210,11 @@ class RelatorioController extends Controller
                 $prev = 0;
                 foreach ($value as $key2 => $value2) {
                     if ($index === 0) {
-                        $value2->total += $saldo;
+                        $value2->totalSomado += $saldo;
                     } else {
-                        $value2->total += $prev;
+                        $value2->totalSomado += $prev;
                     }
-                    $prev = $value2->total;
+                    $prev = $value2->totalSomado;
 
                     $index++;
                 }
