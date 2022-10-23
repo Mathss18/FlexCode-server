934bea203391fbeb2dd5805001a211c0498787e8
unknown <theus.7@hotmail.com>
2022-10-22 23:00:20 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -190,7 +190,7 @@ class RelatorioController extends Controller
             $totalContasBancariasInicial = $totalContasBancariasInicial[0]->totalInicial;
 
             $transacoes = DB::select(DB::raw("SELECT DAY(t.data) as dia, MONTH(t.data) as mes, YEAR(t.data) as ano, SUM(case when t.tipo = 'rendimento' then t.valor else t.valor * -1 end) as total FROM
-             transacoes t AND t.data GROUP BY dia, mes, ano"));
+             transacoes t GROUP BY dia, mes, ano"));
 
 
             dd($transacoes);
