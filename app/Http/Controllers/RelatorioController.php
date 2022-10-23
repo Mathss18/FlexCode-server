268396d5fbde444ff236ccb43fd688e8337a5590
unknown <theus.7@hotmail.com>
2022-10-23 00:29:01 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -199,8 +199,8 @@ class RelatorioController extends Controller
             ksort($valoesPorContaBancaria, SORT_NUMERIC);
 
             foreach ($valoesPorContaBancaria as $key => $value) {
-                $saldoInicial = DB::select(DB::raw("SELECT saldo FROM contas_bancarias WHERE id = $key->idBanco"));
-                dd($saldoInicial);
+                dd($key, $value);
+                // $saldoInicial = DB::select(DB::raw("SELECT saldo FROM contas_bancarias WHERE id = $key->idBanco"));
             }
 
             dd($valoesPorContaBancaria, $contasBancarias);
