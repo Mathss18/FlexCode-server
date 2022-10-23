75a8678c3615d534d1a342c44f6cb19ea16a15b6
unknown <theus.7@hotmail.com>
2022-10-23 02:49:02 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -239,6 +239,7 @@ class RelatorioController extends Controller
             }
 
             foreach ($valoesPorContaBancaria as $key => $value) {
+                dd($valoesPorContaBancaria[$key]);
                 ksort($valoesPorContaBancaria[$key], SORT_LOCALE_STRING);
                 // uasort(
                 //     $valoesPorContaBancaria[$key],
