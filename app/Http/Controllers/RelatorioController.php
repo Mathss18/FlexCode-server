a9145a811dd4174952ce0116d1c7cd6f7272ba94
unknown <theus.7@hotmail.com>
2022-10-22 23:35:04 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -189,8 +189,8 @@ class RelatorioController extends Controller
             $totalContasBancariasInicial = DB::select(DB::raw("SELECT sum(saldoInicial) as totalInicial FROM contas_bancarias"));
             $totalContasBancariasInicial = $totalContasBancariasInicial[0]->totalInicial;
 
-            $transacoes = DB::select(DB::raw("SELECT cb.nome as nomeBanco, DAY(t.data) as dia, MONTH(t.data) as mes, YEAR(t.data) as ano, DATE_FORMAT(t.data,'%d/%m/%Y') as dataFormatada, SUM(case when t.tipo = 'rendimento' then t.valor else t.valor * -1 end) as total FROM
-             transacoes t, contas_bancarias cb WHERE t.data BETWEEN '{$from}' AND '{$to}' AND cb.id = t.conta_bancaria_id GROUP BY dia, mes, ano, cb.nome"));
+            $transacoes = DB::select(DB::raw("SELECT cb.nome as nomeBanco, DATE_FORMAT(t.data,'%d/%m/%Y') as dataFormatada, SUM(case when t.tipo = 'rendimento' then t.valor else t.valor * -1 end) as total FROM
+             transacoes t, contas_bancarias cb WHERE t.data BETWEEN '{$from}' AND '{$to}' AND cb.id = t.conta_bancaria_id GROUP BY DAY(t.data), MONTH(t.data), YEAR(t.data), cb.nome"));
 
 
             $valoesPorContaBancaria = [];
@@ -204,6 +204,7 @@ class RelatorioController extends Controller
                 'datas' => $this->date_range($from, $to, '+1 day', 'd/m/Y'),
                 'valores' => $valoesPorContaBancaria
             ];
+            dd($dados);
 
 
             $response = APIHelper::APIResponse(true, 200, 'Sucesso', $dados);
