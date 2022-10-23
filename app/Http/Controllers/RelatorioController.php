7d97f76fcbce6bd0c76a256cb8f8e7c18d83179a
unknown <theus.7@hotmail.com>
2022-10-22 23:13:41 -0300

    previsao de saldo

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -201,7 +201,9 @@ class RelatorioController extends Controller
                 new DateTime($to)
             );
 
-            dd($period);
+
+
+            dd($this->createDateRangeArray($from, $to));
 
             $acumulador = $totalContasBancariasInicial;
             $dados = [];
@@ -235,5 +237,28 @@ class RelatorioController extends Controller
             $response = APIHelper::APIResponse(false, 500, null, null, $ex);
             return response()->json($response, 500);
         }
+    }
+
+    private function createDateRangeArray($strDateFrom, $strDateTo)
+    {
+        // takes two dates formatted as YYYY-MM-DD and creates an
+        // inclusive array of the dates between the from and to dates.
+
+        // could test validity of dates here but I'm already doing
+        // that in the main script
+
+        $aryRange = [];
+
+        $iDateFrom = mktime(1, 0, 0, substr($strDateFrom, 5, 2), substr($strDateFrom, 8, 2), substr($strDateFrom, 0, 4));
+        $iDateTo = mktime(1, 0, 0, substr($strDateTo, 5, 2), substr($strDateTo, 8, 2), substr($strDateTo, 0, 4));
+
+        if ($iDateTo >= $iDateFrom) {
+            array_push($aryRange, date('Y-m-d', $iDateFrom)); // first entry
+            while ($iDateFrom < $iDateTo) {
+                $iDateFrom += 86400; // add 24 hours
+                array_push($aryRange, date('Y-m-d', $iDateFrom));
+            }
+        }
+        return $aryRange;
     }
 }
