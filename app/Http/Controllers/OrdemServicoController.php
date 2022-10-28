852082585d9e0c1dfad5f76bd7174668b271640e
unknown <theus.7@hotmail.com>
2022-10-28 10:34:01 -0300

    fix ordem-servico-produto

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -162,7 +162,7 @@ class OrdemServicoController extends Controller
 
             // Cadastra os produtos da ordem de serviço
             if ($produtos) {
-                dd($ordensServicos->produtos());
+                dd($ordensServicos->produtos()->get());
                 $ordensServicos->produtos()->detach();
                 foreach ($produtos as $produto) {
                     // DB::table('ordens_servicos_produtos')->where('produto_id', $produto['produto_id'])->delete();
