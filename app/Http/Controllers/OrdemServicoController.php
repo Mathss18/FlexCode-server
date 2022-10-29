ce317d46240cdea6df5a5eb2a5dd0a78f6915179
unknown <theus.7@hotmail.com>
2022-10-29 20:15:05 -0300

    new feature

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -338,9 +338,9 @@ class OrdemServicoController extends Controller
             // Foreach para completar os produtos que um funcionario ainda não iniciou
             foreach ($produtosPorFuncionarios as $produtoPorFuncionario) {
                 foreach ($ordensServicosProdutos as $ordemServicoProduto) {
-                    dd($produtoPorFuncionario['produtos'], $ordemServicoProduto['produto']);
+                    // dd($produtoPorFuncionario['produtos'], $ordemServicoProduto['produto']);
                     if(!$this->in_array_r($ordemServicoProduto['produto']['nome'], $produtoPorFuncionario['produtos'])){
-                        dd('cu');
+                        // dd('cu');
                         array_push($produtoPorFuncionario['produtos'],
                         [
                             'id' =>$ordemServicoProduto['produto']['id'],
