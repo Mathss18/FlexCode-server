c141eac9024b3977f652ecb02dbe5b569c980eeb
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2023-02-22 12:16:16 -0300

    asas

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -533,7 +533,7 @@ class NfeService
     {
         try {
             $xml = $nfe->monta();
-            file_put_contents("xml.xml", $xml);
+            Log::info('xml', [$xml]);
             return $xml;
         } catch (\Exception $ex) {
             $this->error = 'Erro ao montar: ' . $ex->getMessage();
