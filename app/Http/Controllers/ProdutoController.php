c9cd475c28d40d94e7b75a2cb4bad85c91c2ad59
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-02-01 14:08:28 -0300

    fix: paginator

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -14,6 +14,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Facades\JWTAuth;
+Illuminate\Pagination\Paginator;
 
 class ProdutoController extends Controller
 {
