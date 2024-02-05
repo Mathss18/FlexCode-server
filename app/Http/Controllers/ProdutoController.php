faf816062441dc6f3ce84710e59e49b157108530
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-02-05 10:31:34 -0300

    test

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -64,9 +64,8 @@ class ProdutoController extends Controller
             // Add search condition if searchText is provided
             if (!empty($searchText)) {
                 $query->where(function ($q) use ($searchText) {
-                    $q->where('produtos.nome', 'like', '%' . $searchText . '%')
-                        ->orWhere('produtos.codigoInterno', 'like', '%' . $searchText . '%')
-                        ->orWhere('clientes.nome', 'like', '%' . $searchText . '%');
+                    $q->where('produtos.nome', 'like', '%' . $searchText . '%') // Search in the name field
+                        ->orWhere('produtos.codigoInterno', 'like', '%' . $searchText . '%'); // Example: Search in another field
                     // You can add more orWhere clauses to search in other fields or related models
                 });
             }
