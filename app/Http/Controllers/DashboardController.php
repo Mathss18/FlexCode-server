d0fe20d69c0c5aa28fdff1d766e19252064f5d4e
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-04-08 18:20:31 -0300

    rel

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -35,9 +35,11 @@ class DashboardController extends Controller
 
     public function diferencaPercentual()
     {
-        $to = date('Y-m-d H:i:s', strtotime('last day of previous month 23:59:59'));
-        $from = date('Y-m-d H:i:s', strtotime('-12 months', strtotime('first day of previous month')));
+        // Data e hora atuais
+        $to = date('Y-m-d H:i:s');
 
+        // Data e hora de 12 meses atrás
+        $from = date('Y-m-d H:i:s', strtotime('-12 months'));
 
         $query = "SELECT MONTH(v.updated_at) as mes, YEAR(v.updated_at) as ano, SUM(v.total) as total 
                   FROM vendas v 
