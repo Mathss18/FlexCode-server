ccbf6ec40d841e107579d218069d011079f26ab0
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-04-10 19:56:17 -0300

    sd

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -72,7 +72,7 @@ class DashboardController extends Controller
         $averageMonthly = $totalSum / max(count($dados), 1); // To avoid division by zero
         $averageDailyCurrentMonth = ($averageMonthly / 30) * $currentDay;
 
-        var_dump($averageMonthly, $currentDay);
+        var_dump($averageMonthly, $currentDay, $averageDailyCurrentMonth);
 
         return 100 - abs(($currentMonth['balancoFinal'] * 100) / $averageDailyCurrentMonth);
     }
