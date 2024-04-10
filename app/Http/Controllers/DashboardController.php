601123a12ac9a29728067ef5126f3b68c295aa29
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-04-10 18:51:20 -0300

    dfdf

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -46,7 +46,7 @@ class DashboardController extends Controller
                   GROUP BY YEAR(v.updated_at), MONTH(v.updated_at)";
         $transacoes = DB::select(DB::raw($query), ['from' => $from, 'to' => $to]);
 
-        var_dump($transacoes->toArray());
+        var_dump($transacoes);
 
         $dados = [];
         $totalSum = 0; // For calculating total sum
