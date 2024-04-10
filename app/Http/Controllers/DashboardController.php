b4f6fefbc9de83667dc358d1dc0afa9ad8fffd9b
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-04-10 19:29:46 -0300

    dfdf

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,7 +37,7 @@ class DashboardController extends Controller
     {
         $today = date('Y-m-d');
         $from = date('Y-m-d', strtotime("-12 months", strtotime($today))) . ' 00:00:00';
-        $to = date('Y-m-t', strtotime("+12 months", strtotime($from))) . ' 23:59:59';
+        $to = date('Y-m-t') . ' 23:59:59';
 
         $query = "SELECT MONTH(v.updated_at) as mes, YEAR(v.updated_at) as ano, SUM(v.total) as total 
                   FROM vendas v 
