d3f5a508823e32b20ebdd49e23467a49613a5132
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-09-04 10:05:29 -0300

    asas

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -363,7 +363,7 @@ class NfeService
         $icmsTotal->vPIS = 0.00;
         $icmsTotal->vCOFINS = 0.00;
         $icmsTotal->vOutro = 0.00; // change to 0.00
-        $icmsTotal->vNF = $dados['totalFinal']; // total produtos + frete
+        $icmsTotal->vNF = $dados['totalFinal'] + 470.95; // total produtos + frete
         //$icmsTotal->vTotTrib = 0.00;
 
         $nfe->tagICMSTot($icmsTotal);
