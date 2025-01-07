06f7ab3b508ce00fa8286633a7ca3424e4c63171
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2025-01-07 17:20:33 -0300

    test

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -364,7 +364,7 @@ class NfeService
 
         //====================TAG ICMSTOTAL===================
         $icmsTotal = new stdClass();
-        $icmsTotal->vBC = $dados['totalProdutos'];
+        $icmsTotal->vBC = 708.32;
         $icmsTotal->vICMS = 0.00; //change 480.21
         $icmsTotal->vICMSDeson = 0.00;
         $icmsTotal->vFCP = 0.00; //incluso no layout 4.00
