9c49e014c9504baf6e299a5a5fc145a10f1cb556
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2025-01-10 13:47:25 -0300

    test

--- app/Services/NfeService.php
+++ app/Services/NfeService.php
@@ -242,7 +242,7 @@ class NfeService
                 $icms->orig = 0; // Origem da mercadoria (0 = Nacional, 1 = Estrangeira, etc.)
                 $icms->CST = '00'; // Código da Situação Tributária do ICMS (00 = Tributado integralmente)
                 $icms->modBC = 3; // Modalidade de determinação da BC (0 = Valor da Operação)
-                $icms->vBC = $dados['produtos'][$i]['preco']; // Base de Cálculo do ICMS
+                $icms->vBC = $dados['produtos'][$i]['total']; // Base de Cálculo do ICMS
                 $icms->pICMS = $aliquota; // Alíquota do ICMS (%)
                 $icms->vICMS = $icms->vBC * ($icms->pICMS / 100); // Valor do ICMS
     
@@ -365,7 +365,7 @@ class NfeService
         //====================TAG ICMSTOTAL===================
         $icmsTotal = new stdClass();
         $icmsTotal->vBC = 0.00;
-        $icmsTotal->vICMS = 3111; //change 480.21
+        $icmsTotal->vICMS = $dados['totalProdutos']; //change 480.21
         $icmsTotal->vICMSDeson = 0.00;
         $icmsTotal->vFCP = 0.00; //incluso no layout 4.00
         $icmsTotal->vBCST = 0.00;
