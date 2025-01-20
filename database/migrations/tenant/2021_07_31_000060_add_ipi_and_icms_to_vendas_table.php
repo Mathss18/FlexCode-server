12d8b5168426bb4b52798e2b9513f0ec766f9115
Matheus Filho <theus.7@hotmail.com>
2025-01-19 23:45:01 -0300

    chore: add ipi and icms

---
+++ database/migrations/tenant/2021_07_31_000060_add_ipi_and_icms_to_vendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImpostosToOrcamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vendas', function (Blueprint $table) {
+            $table->double('ipi', 9, 4);
+            $table->double('icms', 9, 4);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vendas', function (Blueprint $table) {
+            $table->dropColumn('ipi'); // Drop the column
+            $table->dropColumn('icms'); // Drop the column
+        });
+    }
+}
