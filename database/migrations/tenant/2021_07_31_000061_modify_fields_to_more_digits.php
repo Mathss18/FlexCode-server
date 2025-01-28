b57a2999eb384e089e6a46c660c602e0bcd93dfb
Matheus Filho <theus.7@hotmail.com>
2025-01-28 16:47:19 -0300

    feat: update mediorbis HRA

---
+++ database/migrations/tenant/2021_07_31_000061_modify_fields_to_more_digits.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vendas', function (Blueprint $table) {
+            $table->decimal('total', 12, 4)->change();
+        });
+
+        Schema::table('compras', function (Blueprint $table) {
+            $table->decimal('total', 12, 4)->change();
+        });
+
+        Schema::table('orcamentos', function (Blueprint $table) {
+            $table->decimal('total', 12, 4)->change();
+        });
+
+        Schema::table('notas_fiscais', function (Blueprint $table) {
+            $table->decimal('totalFinal', 12, 4)->change();
+            $table->decimal('totalProdutos', 12, 4)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vendas', function (Blueprint $table) {
+            $table->decimal('total', 9, 4)->change();
+        });
+
+        Schema::table('compras', function (Blueprint $table) {
+            $table->decimal('total', 9, 4)->change();
+        });
+
+        Schema::table('orcamentos', function (Blueprint $table) {
+            $table->decimal('total', 9, 4)->change();
+        });
+
+        Schema::table('notas_fiscais', function (Blueprint $table) {
+            $table->decimal('totalFinal', 9, 4)->change();
+            $table->decimal('totalProdutos', 9, 4)->change();
+        });
+    }
+};
