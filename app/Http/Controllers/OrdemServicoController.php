d8db8ee16670d5d0b3f6a025b2061570ceab4221
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2025-05-09 10:28:32 -0300

    a

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -305,7 +305,7 @@ class OrdemServicoController extends Controller
             foreach ($ordensServicosProdutos as $ordemServicoProduto) {
                 $situacao = json_decode($ordemServicoProduto['situacao']);
                 if($situacao === null){
-                    $situacao = "[]";
+                    $situacao = [];
                 }
                 foreach ($nomesFuncionariosAndIdsFuncionarios as $funcNomeAndId) {
                     foreach ($situacao as $situ) {
